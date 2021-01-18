18768967c16fd37fed8d412710c549a34d691ac0
IamSumukha <sumukh.aithal@gmail.com>
2021-01-18 22:55:46 +0530

    added tab layout

--- app/src/main/java/com/johnnybkotlin/devprofile/MainActivity.kt
+++ app/src/main/java/com/johnnybkotlin/devprofile/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.johnnybkotlin.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val roundedBitmap = RoundedBitmapDrawableFactory.create(resources,bitmap)
+        roundedBitmap.cornerRadius = 25f
+        profileimage.setImageDrawable(roundedBitmap)
+
     }
 }
